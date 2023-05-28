f5bef949ed2771c20ce6238217f33ef8c6d8085d
Naing Linn Khant <nainglinnkhnt@gmail.com>
2023-05-28 21:40:08 +0630

    feat: change standardized object's prop names to camel case

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,12 @@
 import type { TObj } from './App'
+import { camelCase } from 'change-case'
 
 export const standardizeObjectPropNames = (obj: TObj) => {
   const standardizedObj: TObj = {}
   for (const key in obj) {
     // const objKey = key as keyof Data
     // need the above step if obj is a more specific type
-    standardizedObj[key.toLowerCase()] = obj[key]
+    standardizedObj[camelCase(key)] = obj[key]
   }
   return standardizedObj
 }
