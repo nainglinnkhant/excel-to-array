d8ac14516e7e06756b9efbd2f13179e4d83efec9
Naing Linn Khant <nainglinnkhnt@gmail.com>
2023-06-02 02:09:35 +0630

    refactor: move parse functions into utils file

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,20 @@
 import { useRef, useState } from 'react'
-import Papa from 'papaparse'
-import { utils, read } from 'xlsx'
-
-import { standardizeObjectPropNames } from './utils'
-
-export type TObj = Record<string, string>
+import { parseCsv, parseXlsx } from './utils'
 
 function App() {
   const fileInputRef = useRef<HTMLInputElement>(null)
   const [data, setData] = useState<unknown[]>([])
 
-  const parseCsv = (file: File) => {
-    Papa.parse(file, {
-      header: true,
-      skipEmptyLines: true,
-      complete: results =>
-        setData(results.data.map(obj => standardizeObjectPropNames(obj as TObj))),
-    })
-  }
-
-  const parseXlsx = async (file: File) => {
-    const workbook = read(await file.arrayBuffer())
-    const worksheet = workbook.Sheets[workbook.SheetNames[0]]
-
-    const data = utils
-      .sheet_to_json<TObj>(worksheet)
-      .map(row => standardizeObjectPropNames(row))
-
-    setData(data)
-  }
-
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return
 
     const file = e.target.files[0]
     const fileType = file.name.split('.')[1]
 
     if (fileType === 'csv') {
-      parseCsv(file)
+      setData(await parseCsv(file))
     } else if (fileType === 'xlsx') {
-      parseXlsx(file)
+      setData(await parseXlsx(file))
     }
 
     //eslint-disable-next-line @typescript-eslint/no-non-null-assertion
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,8 @@
-import type { TObj } from './App'
+import Papa from 'papaparse'
+import { utils, read } from 'xlsx'
 import { camelCase } from 'change-case'
+
+type TObj = Record<string, string>
 
 export const standardizeObjectPropNames = (obj: TObj) => {
   const standardizedObj: TObj = {}
@@ -9,4 +12,26 @@ export const standardizeObjectPropNames = (obj: TObj) => {
     standardizedObj[camelCase(key)] = obj[key]
   }
   return standardizedObj
+}
+
+export const parseCsv = async (file: File) => {
+  return new Promise<TObj[]>(resolve => {
+    Papa.parse(file, {
+      header: true,
+      skipEmptyLines: true,
+      complete: results =>
+        resolve(results.data.map(obj => standardizeObjectPropNames(obj as TObj))),
+    })
+  })
+}
+
+export const parseXlsx = async (file: File) => {
+  const workbook = read(await file.arrayBuffer())
+  const worksheet = workbook.Sheets[workbook.SheetNames[0]]
+
+  const data = utils
+    .sheet_to_json<TObj>(worksheet)
+    .map(row => standardizeObjectPropNames(row))
+
+  return data
 }
