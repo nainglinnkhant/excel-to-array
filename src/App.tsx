86331351f3d4e9206128442ba7bb2abf51cdf023
Naing Linn Khant <nainglinnkhnt@gmail.com>
2023-06-02 00:33:24 +0630

    feat(ui): add hover effect to button

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ function App() {
   }
 
   return (
-    <div className='mt-8 flex flex-col items-center justify-center gap-6'>
+    <div className='mt-16 flex flex-col items-center justify-center gap-6'>
       <input
         ref={fileInputRef}
         type='file'
@@ -57,7 +57,7 @@ function App() {
       />
 
       <button
-        className='rounded-lg bg-black px-5 py-3 text-sm text-white focus:ring-2 focus:ring-slate-400 focus:ring-offset-2'
+        className='rounded-lg bg-black px-5 py-3 text-sm text-white transition-colors duration-300 ease-out hover:bg-zinc-600 focus:ring-2 focus:ring-slate-400 focus:ring-offset-2'
         onClick={() => fileInputRef.current?.click()}
       >
         Choose File
